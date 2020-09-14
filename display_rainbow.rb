e39c5041fb92b54bdbec887c269726d9b6c96fa4
William Stoneberg <wrstoneberg@me.com>
2020-09-14 09:55:18 +0000

    Automatically backed up by Learn

--- display_rainbow.rb
+++ display_rainbow.rb
@@ -1,5 +1,5 @@
+color_list = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'viloet']
 def display_rainbow(color_list)
-  color_list = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
   puts "R: #{color_list[0]}, O: #{color_list[1]}, Y: #{color_list[2]},
         G: #{color_list[3]}, B: #{color_list[4]}, I: #{color_list[5]},
         V: #{color_list[6]}"
